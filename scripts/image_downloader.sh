3634591f5435831aa6ac2b00e0d750796c6893e3
Gautam Kotian <gautam.kotian@gmail.com>
2016-05-29 07:11:44 +0200

    Fix date typo in image downloader

--- scripts/image_downloader.sh
+++ scripts/image_downloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DATE=2012-02-19
+DATE=2010-02-19
 
 # Example line
 #curl -o ${PLAY_DIR}/website/assets/travels/${DATE}-sunrise.jpg https://googs123.files.wordpress.com/2010/02/sam_0337.jpg
