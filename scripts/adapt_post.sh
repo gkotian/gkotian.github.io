aeb8ba0a970c8e65367cce4f7f36b31eae669d7d
Gautam Kotian <gautam.kotian@convex-energy.com>
2025-12-27 22:03:49 +0100

    Adapt post "Medellin"

--- scripts/adapt_post.sh
+++ scripts/adapt_post.sh
@@ -11,38 +11,40 @@ function waitForConfirmation
     echo ""
 }
 
-POST_TO_ADAPT=`basename $1`
+# POST_TO_ADAPT=`basename $1`
 
-FILENAME_WITHOUT_EXTN="${POST_TO_ADAPT%.*}"
+# FILENAME_WITHOUT_EXTN="${POST_TO_ADAPT%.*}"
 
-TARGET_FILE=${PLAY_DIR}/websites/gautam-kotian/travels/_posts/${FILENAME_WITHOUT_EXTN}.md
+# TARGET_FILE=${PLAY_DIR}/websites/gautam-kotian/travels/_posts/${FILENAME_WITHOUT_EXTN}.md
 
-git mv ${PLAY_DIR}/websites/gautam-kotian/_posts/${POST_TO_ADAPT} ${TARGET_FILE}
+# git mv ${PLAY_DIR}/websites/gautam-kotian/_posts/${POST_TO_ADAPT} ${TARGET_FILE}
 
-# Substitute the ’ character with a single apostrophe
-sed -i 's/’/'\''/g' ${TARGET_FILE}
+# # Substitute the ’ character with a single apostrophe
+# sed -i 's/’/'\''/g' ${TARGET_FILE}
 
-# Substitute the “ or ” characters with a double apostrophe
-sed -i 's/“/"/g' ${TARGET_FILE}
-sed -i 's/”/"/g' ${TARGET_FILE}
+# # Substitute the “ or ” characters with a double apostrophe
+# sed -i 's/“/"/g' ${TARGET_FILE}
+# sed -i 's/”/"/g' ${TARGET_FILE}
 
-# Substitute the – character with a simple hyphen
-sed -i 's/–/-/g' ${TARGET_FILE}
+# # Substitute the – character with a simple hyphen
+# sed -i 's/–/-/g' ${TARGET_FILE}
 
-# Substitute the ellipsis character with three dots
-sed -i 's/…/.../g' ${TARGET_FILE}
+# # Substitute the ellipsis character with three dots
+# sed -i 's/…/.../g' ${TARGET_FILE}
 
-# Delete `<p style="text-align:center;">` lines
-sed -i '/^<p style="text-align:center;">$/d' ${TARGET_FILE}
+# # Delete `<p style="text-align:center;">` lines
+# sed -i '/^<p style="text-align:center;">$/d' ${TARGET_FILE}
 
-echo "Open '${TARGET_FILE}' and do the following:"
-echo "    - remove useless stuff in frontmatter (but keep categories)"
-echo "    - remove useless stuff at the end"
-echo "    - add blank line after frontmatter"
-echo "    - remove '<p>' & '</p>' tags"
-echo "    - remove other HTML"
-echo "    - add an excerpt"
-waitForConfirmation
+# echo "Open '${TARGET_FILE}' and do the following:"
+# echo "    - remove useless stuff in frontmatter (but keep categories)"
+# echo "    - remove useless stuff at the end"
+# echo "    - add blank line after frontmatter"
+# echo "    - remove '<p>' & '</p>' tags"
+# echo "    - remove other HTML"
+# echo "    - add an excerpt"
+# waitForConfirmation
+
+TARGET_FILE=/home/gautam/play/websites/gautam-kotian/travels/_posts/2010-06-19-medellin.md
 
 ARR=(`grep "href=" ${TARGET_FILE} | sed 's|^.*caption="\(.*\)".*href="\(.*\)">.*$|\1ö\2|g' | sed 's|\s|-|g' | sed 's|öhttp| https|g'`)
 
@@ -88,7 +90,7 @@ do
     IMAGE_FILE_NAME=${DATE}-${SHORT_NAME}
     IMAGE_FILE_PATH=${PLAY_DIR}/websites/gautam-kotian/assets/travels/${IMAGE_FILE_NAME}.${EXTENSION}
 
-    curl -o ${IMAGE_FILE_PATH} ${IMAGE_URL} 2> /dev/null
+    curl -Lo ${IMAGE_FILE_PATH} ${IMAGE_URL} 2> /dev/null
 
     echo "${IMAGE_FILE_NAME}.${EXTENSION}"
 done
